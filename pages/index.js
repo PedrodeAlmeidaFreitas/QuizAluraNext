5773ac74eb83e53032f1c9cb14174589e0e9e588
Pedro Almeida <pedro.a.freitas.1992@gmail.com>
2021-01-28 19:07:37 -0300

    Base projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget/widget'
+import Footer from '../src/components/Footer/footer'
+import GitHubCorner from '../src/components/GitHubCorner/gitHubCorner'
+import QuizBackground from '../src/components/QuizBackground/quizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
-export default function Home() {
-  return <Title>My page</Title>
+const Home = () => {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Titulo</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Pergunta</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl=""/>
+    </QuizBackground>
+  );
 }
+
+export default Home;
